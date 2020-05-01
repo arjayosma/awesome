4c288fe6ebbdcea93d7e60bcefa20af1dddf982d
Arjay Osma <arjay.osma@gmail.com>
2020-05-01 19:41:50 +0800

    Update on SEO tags

--- src/components/seo.js
+++ src/components/seo.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types"
 import Helmet from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
-const SEO = ({ description, lang, meta, slug, title }) => {
+const SEO = ({ description, lang, meta, title }) => {
   const { site } = useStaticQuery(
     graphql`
       query {
@@ -38,7 +38,7 @@ const SEO = ({ description, lang, meta, slug, title }) => {
         },
         {
           property: "og:url",
-          content: `${siteUrl}${slug}`,
+          content: `${siteUrl}`,
         },
         {
           property: `og:title`,
