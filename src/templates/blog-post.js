2bb79635a3d10009a7133eb00c9ec9032a64aaf3
Arjay Osma <arjay.osma@gmail.com>
2020-05-02 13:59:49 +0800

    Improvement of SEO tags

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -14,7 +14,7 @@ const BlogPostTemplate = ({ data, pageContext, location }) => {
   return (
     <Layout location={location} title={siteTitle}>
       <SEO
-        title={post.frontmatter.title}
+        title={`${post.frontmatter.title} | ${siteTitle}`}
         description={post.frontmatter.description || post.excerpt}
         slug={slug}
       />
